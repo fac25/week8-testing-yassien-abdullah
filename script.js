e5dced394522a1b9ddf97c5a44df212cc275fdf2
flare-s <webflarez@gmail.com>
2022-07-10 15:42:10 +0100

    Added showing and hiding lists styles and functionality

--- script.js
+++ script.js
@@ -30,6 +30,25 @@ const tasksArray = [
     }
 ]
 
+// Show or hide the list section depending if it has tasks or not
+const showOrHideSection = () => {
+    if(//i.e if there are no more incomplete tasks
+        document.getElementById("theList").childElementCount == 0
+    ){//remove the relevant h2's text
+        document.querySelector(".unfinished-section").classList.add("hidden")
+    } else {
+        document.querySelector(".unfinished-section").classList.remove("hidden")
+    }
+
+    if(//i.e if there are no more completed tasks
+        document.getElementById("theCompletedTasks").childElementCount == 0
+    ){//remove the relevant h2's text
+        document.querySelector(".completed-section").classList.add("hidden")
+    } else {
+        document.querySelector(".completed-section").classList.remove("hidden")
+    }
+}
+
 // Create task
 const createTask = (text, id) => {
     // Create a task li element wit an id
@@ -62,8 +81,7 @@ const createId = (array) => {
     return array[array.length - 1].id + 1;
 }
 
-//variable to be incremented after each task is successfully added
-// let taskNumber = 0;
+
 
 // Loop through the default tasks and show them on their respective lists
 for (let i = 0; i < tasksArray.length; i++) {
@@ -84,6 +102,8 @@ for (let i = 0; i < tasksArray.length; i++) {
         toDoList.append(item);
     }
 }
+
+showOrHideSection();
 
 function addTask(event) {
     event.preventDefault();
@@ -115,101 +135,14 @@ function addTask(event) {
     //empty the textarea for the subsequent tasks to be added
     document.getElementById("taskToAdd").value = "";
     console.log(tasksArray)
+    showOrHideSection();
 }
 
-// function addTask(event) {
-//     event.preventDefault();
-
-//     //increment the taskCounter
-//     taskNumber++;
-
-//     //get the task
-//     let taskUserWantsToAdd = document.getElementById("taskToAdd").value;
-//     console.log(taskUserWantsToAdd);
-
-//     //get the list
-//     const toDoList = document.getElementById("theList");
-//     console.log(toDoList);
-
-//     //now that a task has been added, write into the relevant h2
-//     let h2 = document.getElementById("h2ForIncompleteTasks");
-//     h2.innerHTML = "Tasks yet to be done";
-
-//     //add task, checkbox and delete button to the list
-//     toDoList.innerHTML += `<li id="Task${taskNumber}">` +
-//     `<input type="checkbox" id="checkboxForTask${taskNumber}"` + 
-//     `aria-label="Mark this task as completed" class="checkbox">` + 
-//     `<span id="task${taskNumber}Text">${taskUserWantsToAdd}</span>` +
-//     `<span id="deleteTask${taskNumber}" class="delete"` + 
-//     `aria-label="Delete this task from the to-do list">&times;</span>` + 
-//     `</li>`
-
-
-//     /*loop through all the complete buttons and add event listeners for them so they can 
-//     be marked as complete when clicked
-
-//     Note: When you attach an event handler inline, eg onclick=“markCompleted()”, 
-//     within the function "this" refers to window. so basically use event handlers IN 
-//     the js file instead of onclick, onchange etc in the html element if you want to 
-//     get the value or id of “this” otherwise you just get undefined
-    
-//     Note2: For some reason the buttons dont work if they event listeners are added 
-//     AFTER/outside the addTask fn scope*/
-//     const checkboxes = document.querySelectorAll(".checkbox");
-//     for (let i = 0; i < checkboxes.length; i++) {
-//       checkboxes[i].addEventListener("click", markCompleted); 
-//     }
-    
-//     //same again for the delete "buttons"
-//     const deleteButtons = document.querySelectorAll(".delete");
-//     for (let i = 0; i < deleteButtons.length; i++) {
-//       deleteButtons[i].addEventListener("click", deleteFromList);
-//     }
-
-//     //empty the textarea for the subsequent tasks to be added
-//     document.getElementById("taskToAdd").value = "";
-// }
 
 
 
 
 
-// function markCompleted(){
-//     console.log(this.id);
-
-//     //first get the task number for which the checkbox was clicked
-//     let taskNumberForClickedCheckbox = this.id.slice(15);
-//     console.log(taskNumberForClickedCheckbox);
-
-//     //then get the span containing the task text
-//     let idOfSpanContainingTaskText = `task${taskNumberForClickedCheckbox}Text`;
-//     let taskTextSpan = document.getElementById(idOfSpanContainingTaskText);
-    
-//     //then add the completed-task class to that span
-//     taskTextSpan.className += "completed-task";
-
-//     //then remove the checkbox 
-//     this.parentNode.removeChild(this);
-
-//     //then move the completed task below the non-completed tasks
-//     let listOfCompletedTasks = document.getElementById("theCompletedTasks");
-//     let idOfTaskToMove = `Task${taskNumberForClickedCheckbox}`;
-//     let taskToMove = document.getElementById(idOfTaskToMove);
-//     listOfCompletedTasks.appendChild(taskToMove);
-
-//     //now that a task has been marked as complete, write into the relevant h2
-//     let h2 = document.getElementById("h2ForCompletedTasks");
-//     h2.innerHTML = "Completed tasks";
-
-//     if(//i.e if there are no more incomplete tasks
-//         document.getElementById("theList").childElementCount == 0
-//     ){//remove the relevant h2's text
-//         document.getElementById("h2ForIncompleteTasks").innerHTML = ""
-//     }
-   
-
-    
-// }
 
 
 function markCompleted(){
@@ -220,9 +153,6 @@ function markCompleted(){
     // Find the index of the task in the task array that matches the number if the id of the list item
     let ind = tasksArray.findIndex(el => el.id === Number(id));
     if (this.checked) {
-        //now that a task has been marked as complete, write into the relevant h2
-        let h2 = document.getElementById("h2ForCompletedTasks");
-        h2.innerHTML = "Completed tasks";
         // Change the completed state if the task inside the tasks array to reflect the change
         tasksArray[ind].completed = true;
 
@@ -234,9 +164,6 @@ function markCompleted(){
             toDoList.removeChild(this.parentElement);
         }
     } else {
-        //now that a task has been marked as incomplete, write into the relevant h2
-        let h2 = document.getElementById("h2ForIncompleteTasks");
-        h2.innerHTML = "Tasks yet to be done";
         // Change the completed state if the task inside the tasks array to reflect the change
         tasksArray[ind].completed = false;
         this.parentElement.querySelector('label').classList.remove("completed-task");
@@ -247,48 +174,14 @@ function markCompleted(){
             listOfCompletedTasks.removeChild(this.parentElement);
         }
     }
-
-    if(//i.e if there are no more incomplete tasks
-        document.getElementById("theList").childElementCount == 0
-    ){//remove the relevant h2's text
-        document.getElementById("h2ForIncompleteTasks").innerHTML = ""
-    }
-
-    if(//i.e if there are no more completed tasks
-        document.getElementById("theCompletedTasks").childElementCount == 0
-    ){//remove the relevant h2's text
-        document.getElementById("h2ForCompletedTasks").innerHTML = ""
-    }
+    showOrHideSection();
    
 
     
 }
 
 
-// function deleteFromList() {
-//     console.log(this.id);
 
-//     let idOfElementToBeRemoved = this.id.slice(6);
-//     console.log(idOfElementToBeRemoved);
-
-//     const elementToRemove = document.getElementById(idOfElementToBeRemoved);
-//     elementToRemove.parentNode.removeChild(elementToRemove);
-
-//     if(//i.e if there are no more incomplete tasks
-//         document.getElementById("theList").childElementCount == 0
-//     ){//remove the relevant h2's text
-//         document.getElementById("h2ForIncompleteTasks").innerHTML = ""
-//     }
-
-//     if(//i.e if there are no more completed tasks
-//         document.getElementById("theCompletedTasks").childElementCount == 0
-//     ){//remove the relevant h2's text
-//         document.getElementById("h2ForCompletedTasks").innerHTML = ""
-//     }
-   
-
-
-// }
 
 
 function deleteFromList() {
@@ -305,16 +198,6 @@ function deleteFromList() {
     const elementToRemove = document.getElementById(this.parentElement.id);
     elementToRemove.parentElement.removeChild(elementToRemove);
 
-    if(//i.e if there are no more incomplete tasks
-        document.getElementById("theList").childElementCount == 0
-    ){//remove the relevant h2's text
-        document.getElementById("h2ForIncompleteTasks").innerHTML = ""
-    }
-
-    if(//i.e if there are no more completed tasks
-        document.getElementById("theCompletedTasks").childElementCount == 0
-    ){//remove the relevant h2's text
-        document.getElementById("h2ForCompletedTasks").innerHTML = ""
-    }
+    showOrHideSection();
 
 }
